4877c6930a7c743662d3cda6caa3f44849e8a063
Muhammad Ismail Yusuf <mismailyusuf@gmail.com>
2019-10-08 06:52:32 +0700

    add ssl

--- server/app.js
+++ server/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const enrouten = require('express-enrouten');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 app.use(cors());
 app.use(express.json());
@@ -15,9 +17,15 @@ app.use('*', (req, res) => res.redirect(process.env.WEBSITE_URL));
 
 mongoose.connect(`${process.env.MONGO_DB}`, { useNewUrlParser: true }, (err) => {
   if (!err) return console.log('Connected to the database');
-  return console.log('Not connected to the database, an error has occured: ', err.message || err)
+  return console.log('Not connected to the database, an error has occured: ', err.message || err);
 });
 
-app.listen(port, () => console.log(`Service is running on port ${port}`));
+const options = {
+  key: fs.readFileSync('./ssl/privatekey.pem'),
+  cert: fs.readFileSync('./ssl/certificate.pem'),
+};
 
-module.exports = app;
+const server = https.createServer(options, app);
+server.listen(port, () => console.log(`Service is running on port ${port}`));
+
+module.exports = server;
